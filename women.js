c39420d4d4945940d5f54c98ea5ed5971b8b9e67
priyanshupawar <talk20priyanshu@gmail.com>
2022-07-24 05:40:20 +0530

    New data added in women page

---
+++ women.js
@@ -0,0 +1,542 @@
+let womensData = [
+  {
+    image: "https://m.media-amazon.com/images/I/61hHzoAOjzL._AC_UL320_.jpg",
+    productName: "Women's Green Poly Chiffon Kurta",
+    productPrice: "829",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81hkyvQwYyS._AC_UL320_.jpg",
+    productName: "Women's Blue Poly Georgette Dress",
+    productPrice: "863",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71omn81gAAL._AC_UL320_.jpg",
+    productName: "Women's Off-White Poly Silk Kurta With Pant And Dupatta",
+    productPrice: "788",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71SRhc6ZkRL._AC_UL320_.jpg",
+    productName: "Women's Light Brown Poly Silk Straight Kurta",
+    productPrice: "329",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81uJHOVq7GL._AC_UL320_.jpg",
+    productName: "Women's Pure Cotton Straight Kurta",
+    productPrice: "519",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71vEK36NGtL._AC_UL320_.jpg",
+    productName: "Women's Crepe Regular Kurta",
+    productPrice: "358",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71wQ3t7L2xL._AC_UL320_.jpg",
+    productName: "Women Salwar Suit",
+    productPrice: "899",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71sOLoj8llL._AC_UL320_.jpg",
+    productName: "Women's Pink Poly Silk Kurta With Pant",
+    productPrice: "719",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81JEyvXxwFL._AC_UL320_.jpg",
+    productName: "Women's Polyester Regular Kurta Set",
+    productPrice: "1,299",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71klqAebCjL._AC_UL320_.jpg",
+    productName: "Women's Multicolor Poly Crepe Asymmetric Kurta",
+    productPrice: "359",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71ANgCYp53L._AC_UL320_.jpg",
+    productName: "Women's Poly Crepe Flared Kurta",
+    productPrice: "399",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71aZ6jFYvDL._AC_UL320_.jpg",
+    productName: "Women's Poly Silk Anarkali Kurta",
+    productPrice: "498",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81M9ZXMuCUL._AC_UL320_.jpg",
+    productName: "Women's Blue Pure Cotton Kurta",
+    productPrice: "749",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/91DZMtE0gFL._AC_UL320_.jpg",
+    productName: "Women's Cotton Blend Solid Half Sleeves Readymade Blouse",
+    productPrice: "339",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81YjpfIfvmL._AC_UL320_.jpg",
+    productName:
+      "Women's Mustard Poly Crepe Straight Kurta With Pant And Dupatta",
+    productPrice: "759",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/913GN3zYDLL._AC_UL320_.jpg",
+    productName:
+      "Women's Cotton Lycra Stretchable Readymade Blouse (BL057-65BLACK-A_Black_FreeSize)",
+    productPrice: "379",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61-pdYDWBOL._AC_UL320_.jpg",
+    productName: "Women's Teal Poly Chiffon Kurta",
+    productPrice: "770",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71rq275rIGL._AC_UL320_.jpg",
+    productName: "Women's Kurta",
+    productPrice: "499",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/91IQBbQieuL._AC_UL320_.jpg",
+    productName:
+      "Women's Cotton Lycra Stretchable Readymade Blouse (BL035-161GOLD)",
+    productPrice: "379",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/91WW9LpR3LL._AC_UL320_.jpg",
+    productName:
+      "Women's Cotton Lycra Stretchable Readymade Blouse (BL013-62BLACK)",
+    productPrice: "379",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/91ybbMQdz9L._AC_UL320_.jpg",
+    productName:
+      "Women's Cotton Lycra Stretchable Readymade Blouse (BL021-71BLACK)",
+    productPrice: "409",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81LjC+EwrVL._AC_UL320_.jpg",
+    productName: "Women's Turquoise Poly Crepe A-line Kurta",
+    productPrice: "348",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71P+nBzRKBL._AC_UL320_.jpg",
+    productName: "Women's Cotton Straight Kurta",
+    productPrice: "469",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71EgZQtnb9L._AC_UL320_.jpg",
+    productName: "Women's Teal Poly Crepe High Low Kurta",
+    productPrice: "439",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61txrrG+hoL._AC_UL320_.jpg",
+    productName: "Women's Pink Poly Chiffon Kurta",
+    productPrice: "770",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71q98CFoNFL._AC_UL320_.jpg",
+    productName: "Women's Poly Silk Kurta With Pant And Art Silk Dupatta",
+    productPrice: "666",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81eKK058-1L._AC_UL320_.jpg",
+    productName: "Women's White Weaved Cotton A-Line Top",
+    productPrice: "589",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71bTRIA3C4L._AC_UL320_.jpg",
+    productName: "Women's Turquoise Blue Poly Crepe A-line Kurta",
+    productPrice: "449",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71hmSXx-joL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Crepe Kurta",
+    productPrice: "369",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71U3VL8CniL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Crepe Flared Kurta",
+    productPrice: "345",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81y+w3gZnZL._AC_UL320_.jpg",
+    productName: "Women's Blue Cotton Straight Kurta with Pant And Dupatta",
+    productPrice: "1,009",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81c8E2byjYL._AC_UL320_.jpg",
+    productName: "Women's Black Rayon Kurta With Dupatta",
+    productPrice: "939",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81Pmocf7L4L._AC_UL320_.jpg",
+    productName: "Women's Regular Fit Tunic",
+    productPrice: "614",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81NwkJIsygS._AC_UL320_.jpg",
+    productName: "Women's Black Cotton Western Dress with Pocket",
+    productPrice: "659",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/8125z7oQKpL._AC_UL320_.jpg",
+    productName: "Women's White Cotton Kurta",
+    productPrice: "469",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71d9PLUp59L._AC_UL320_.jpg",
+    productName: "Women's Black Cotton Flex Kurta",
+    productPrice: "385",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71PQugMmzLL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Crepe Kurta With Pant And Dupatta",
+    productPrice: "761",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/816rDBqt8NL._AC_UL320_.jpg",
+    productName: "Women's Poly Silk Gold Kurta With Pant And Dupatta",
+    productPrice: "899",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71qvw3zE49L._AC_UL320_.jpg",
+    productName: "Women's Crepe Straight Salwar Suit Set",
+    productPrice: "788",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/7140ABYo7sL._AC_UL320_.jpg",
+    productName: "Women's Crepe a-line Kurta",
+    productPrice: "379",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81YeqUx8tnS._AC_UL320_.jpg",
+    productName: "Women's Green Poly Georgette Top",
+    productPrice: "659",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/815N9z2V-7L._AC_UL320_.jpg",
+    productName: "Women's Red Weaved Cotton A-Line Top",
+    productPrice: "569",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71vEK36NGtL._AC_UL320_.jpg",
+    productName: "Women's Crepe Regular Kurta",
+    productPrice: "358",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81X5RIT-Z6L._AC_UL320_.jpg",
+    productName: "Women Kurta Set",
+    productPrice: "759",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71PfL-MfFuL._AC_UL320_.jpg",
+    productName: "Women's Rayon Floral Print Flared Gown",
+    productPrice: "799",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71wQ3t7L2xL._AC_UL320_.jpg",
+    productName: "Women Salwar Suit",
+    productPrice: "899",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81JEyvXxwFL._AC_UL320_.jpg",
+    productName: "Women's Polyester Regular Kurta Set",
+    productPrice: "1,299",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71klqAebCjL._AC_UL320_.jpg",
+    productName: "Women's Multicolor Poly Crepe Asymmetric Kurta",
+    productPrice: "359",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71aZ6jFYvDL._AC_UL320_.jpg",
+    productName: "Women's Poly Silk Anarkali Kurta",
+    productPrice: "498",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81YjpfIfvmL._AC_UL320_.jpg",
+    productName:
+      "Women's Mustard Poly Crepe Straight Kurta With Pant And Dupatta",
+    productPrice: "759",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71VnnPCc5XL._AC_UL320_.jpg",
+    productName: "Women's Teal Straight Poly Crepe Kurta",
+    productPrice: "329",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61-pdYDWBOL._AC_UL320_.jpg",
+    productName: "Women's Teal Poly Chiffon Kurta",
+    productPrice: "770",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71rq275rIGL._AC_UL320_.jpg",
+    productName: "Women's Kurta",
+    productPrice: "499",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61txrrG+hoL._AC_UL320_.jpg",
+    productName: "Women's Pink Poly Chiffon Kurta",
+    productPrice: "770",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71q98CFoNFL._AC_UL320_.jpg",
+    productName: "Women's Poly Silk Kurta With Pant And Art Silk Dupatta",
+    productPrice: "666",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71bTRIA3C4L._AC_UL320_.jpg",
+    productName: "Women's Turquoise Blue Poly Crepe A-line Kurta",
+    productPrice: "449",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71U3VL8CniL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Crepe Flared Kurta",
+    productPrice: "345",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71M9eag73FL._AC_UL320_.jpg",
+    productName: "Women's Poly Crepe Flared Kurta",
+    productPrice: "379",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81y+w3gZnZL._AC_UL320_.jpg",
+    productName: "Women's Blue Cotton Straight Kurta with Pant And Dupatta",
+    productPrice: "1,009",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71PQugMmzLL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Crepe Kurta With Pant And Dupatta",
+    productPrice: "761",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71d9PLUp59L._AC_UL320_.jpg",
+    productName: "Women's Black Cotton Flex Kurta",
+    productPrice: "385",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71qvw3zE49L._AC_UL320_.jpg",
+    productName: "Women's Crepe Straight Salwar Suit Set",
+    productPrice: "788",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/7140ABYo7sL._AC_UL320_.jpg",
+    productName: "Women's Crepe a-line Kurta",
+    productPrice: "379",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71igZ0MzBZL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Silk Kurta With Pant",
+    productPrice: "639",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/714z48j81jL._AC_UL320_.jpg",
+    productName: "Women's Crepe A-line Kurta with Pant",
+    productPrice: "574",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71d+8tRifmL._AC_UL320_.jpg",
+    productName: "Women's Peach Poly Crepe Flared Kurta With Dhoti Pant",
+    productPrice: "681",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81s0Toi6k5L._AC_UL320_.jpg",
+    productName: "Women's Green Poly Silk Kurta With Pant And Dupatta",
+    productPrice: "801",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81eOlKNaVAL._AC_UL320_.jpg",
+    productName: "Women's Green Cotton Straight Kurta with Pant",
+    productPrice: "939",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71-pBIAoKPL._AC_UL320_.jpg",
+    productName: "Women's Cotton Kurta With Pant And Dupatta",
+    productPrice: "850",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81njzyjUQdL._AC_UL320_.jpg",
+    productName:
+      "Women's Brown Poly Silk Foil Print Kurta with Pant and Dupatta",
+    productPrice: "845",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/719IB86rdEL._AC_UL320_.jpg",
+    productName: "Women's Light Pink Cotton Kurta with Pant and Dupatta",
+    productPrice: "1,325",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71VQ0SBMwBL._AC_UL320_.jpg",
+    productName: "Women's Pink Poly Silk Kurta With Pant And Dupatta",
+    productPrice: "852",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71X9Cw9Kn1L._AC_UL320_.jpg",
+    productName: "Women's Cream Cotton Kaftan With Pant",
+    productPrice: "899",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71yxnjlgZEL._AC_UL320_.jpg",
+    productName: "Women's Yellow Poly Crepe A-line Kurta",
+    productPrice: "380",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71NeBW-y8dL._AC_UL320_.jpg",
+    productName: "Women Salwar Suit Set",
+    productPrice: "849",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81z15P5DBcL._AC_UL320_.jpg",
+    productName: "Women's Pink Pure Cotton Straight Kurta With Pant",
+    productPrice: "764",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71w4BDP8yBL._AC_UL320_.jpg",
+    productName: "Indian Women's Navy Blue Poly Crepe Kurta with Dhoti Pant",
+    productPrice: "599",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81gJTYkD8DL._AC_UL320_.jpg",
+    productName: "Women's Navy Blue Cotton Straight Kurta with Pant",
+    productPrice: "939",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81ZAE9YAmNL._AC_UL320_.jpg",
+    productName: "Women's Poly Crepe Frontslit Kurta",
+    productPrice: "279",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71sY6lN6XGL._AC_UL320_.jpg",
+    productName: "Women's Poly Silk Kurta With Pant",
+    productPrice: "666",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71uqiYq9auL._AC_UL320_.jpg",
+    productName: "Women's Teal Poly Chiffon Western Dress",
+    productPrice: "770",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81iVb-xEmaL._AC_UL320_.jpg",
+    productName:
+      "Women's Light Brown Poly Chanderi Kurta With Pant and Dupatta",
+    productPrice: "1,295",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/31g3dXyDReL._AC_UL320_.jpg",
+    productName: "Women's Pink Poly Crepe Flared Kurta",
+    productPrice: "273",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71Ajh8F8odL._AC_UL320_.jpg",
+    productName: "Women's Green Poly Silk Kurta With Pant",
+    productPrice: "639",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61VfQpZ02qL._AC_UL320_.jpg",
+    productName: "Women's Maroon Poly Crepe Kurta With Pant And Dupatta",
+    productPrice: "761",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71b2nic1AsL._AC_UL320_.jpg",
+    productName: "Women's Black Poly Silk Straight Kurta with Pant",
+    productPrice: "724",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81qgA89ncdS._AC_UL320_.jpg",
+    productName: "Women's Green Poly Crepe Kurta",
+    productPrice: "301",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71lls0NI-TL._AC_UL320_.jpg",
+    productName: "Women's Green Poly Crepe Kurta",
+    productPrice: "335",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71n5T56cYNL._AC_UL320_.jpg",
+    productName: "Women's Black Poly Crepe A-line Kurta",
+    productPrice: "439",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81-hVAKRIgL._AC_UL320_.jpg",
+    productName: "Women's Yellow Cotton A-Line Kurta with Pant and Dupatta",
+    productPrice: "1,229",
+  },
+];
+
+let addTodata = [];
+DisplymensFunc(womensData);
+
+function DisplymensFunc(womensData) {
+  womensData.forEach(function (ele, index) {
+    let div = document.createElement("div");
+    let image = document.createElement("img");
+    image.setAttribute("src", ele.image);
+    let heading = document.createElement("h4");
+    heading.innerText = ele.productName;
+    let price = document.createElement("h5");
+    price.innerText = ele.productPrice;
+    let btn = document.createElement("button");
+    btn.innerText = "Add To Cart";
+    // btn.addEventListener("click", addtocartFunc);
+
+    div.append(image, heading, price, btn);
+    document.querySelector("#container").append(div);
+  });
+
+  // function addtocartFunc(ele) {
+  //   addTodata.push(event.target.ele.parentNode());
+  // }
+  // console.log(addTodata);
+}
+
+let imglink = [
+  "https://images-static.nykaa.com/uploads/tr:w-3038,/93c65f95-ee69-42c8-9873-85ec7aed26a0.jpg",
+  "https://images-static.nykaa.com/uploads/tr:w-3038,/ae6ac9e0-f8a5-4410-986f-a3413a965d5a.jpg",
+  "https://images-static.nykaa.com/uploads/tr:w-3038,/e19ffd32-38ba-4e5b-886c-7d2694ac6670.jpg",
+  "https://images-static.nykaa.com/uploads/tr:w-3038,/bdf548e3-8836-4a5e-93f1-423624195213.jpg",
+];
+
+let crauser = document.getElementById("crauser");
+let leftbtn = document.getElementById("left-btn");
+let rightbtn = document.getElementById("right-btn");
+
+let imgstate = 0;
+rightbtn.addEventListener("click", function () {
+  imgstate++;
+  if (imgstate === imglink.length) {
+    imgstate = 0;
+  }
+  crauser.src = imglink[imgstate];
+});
+leftbtn.addEventListener("click", function () {
+  imgstate--;
+  if (imgstate < 0) {
+    imgstate = imglink.length - 1;
+  }
+  crauser.src = imglink[imgstate];
+});
+
+// document.querySelector("#filterPrice").addEventListener("change", handlePrice);
+
+// function handlePrice() {
+//   let selected = document.querySelector("#filterPrice").value;
+//   // console.log(selected);
+//   if (selected == "Accending") {
+//     mensData.sort(function (a, b) {
+//       let x = a.price.toUpperCase();
+//       let y = b.price.toUpperCase();
+//       if (x > y) return 1;
+//       if (x < y) return -1;
+//       return 0;
+//     });
+//     console.log(mensData);
+//     DisplymensFunc(mensData);
+//   }
+// if (selected == "Decending") {
+//   mensData.sort(function (a, b) {
+//     let x = a.price.toUpperCase();
+//     let y = b.price.toUpperCase();
+//     if (x > y) return -1;
+//     if (x < y) return 1;
+//     return 0;
+//   });
+//   console.log(mensData);
+//   DisplymensFunc(mensData);
+// }
+// }
